45bd06a2bb3c0493a1f198198c3bdf49970be363
rurikjart <rurik.j.art@gmail.com>
2022-08-15 16:17:36 +0300

    6. Dependency Injection. Знакомство

---
+++ src/main/java/com/ut/spring/spring_introduction/Test3.java
@@ -0,0 +1,19 @@
+package com.ut.spring.spring_introduction;
+
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test3
+{
+    public static void  main(String[] args) {
+
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Pet pet = new Dog();
+        Person person = new Person(pet);
+        person.callYourPet();
+
+        context.close();
+    }
+}
