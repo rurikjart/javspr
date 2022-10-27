f168a1270932cb3ad77b98471b9475909231df7e
rurikjart <rurik.j.art@gmail.com>
2022-10-27 16:57:29 +0300

    21. Конфигурация с помощью Java code. Способ 1
    21. Конфигурация с помощью Java code. Способ 1

---
+++ src/main/java/com/ut/spring/spring_introduction/MyConfig.java
@@ -0,0 +1,10 @@
+package com.ut.spring.spring_introduction;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.ut.spring.spring_introduction")
+public class MyConfig {
+}
